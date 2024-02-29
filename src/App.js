9828de9d1e03ff0eb0246fa28f33a44b2f7b3f5a
tynan Sampsel <tynansampselr@gmail.com>
2024-02-28 23:03:16 -0400

    basic functionality added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          hi
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			
+			<div className="main-content">
+				<div className="nav-bar">
+					<h1 className="nav-title">ENTERVUE</h1>
+					<h1 className="coming-soon">Coming soon!</h1>
+				</div>
+				{/* <h2 className="title-name">EnterVue</h2> */}
+				<h1 className="title">Coming Soon!</h1>
+				<p className="text">
+				Exciting things are happening behind the scenes at EnterVue! Our small but dedicated team is hard at work crafting a revolutionary platform designed to streamline the interview process for companies of all sizes.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
